49a09f61ade8bde6aed57d7c69ad3bdfa2230d97
7-Spades <edwin5106@gmail.com>
2020-05-12 17:50:29 -0500

    component joiner

---
+++ my-react/src/components/content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Imgheader from './header'
+import Employeetable from './table'
+
+function Content (){
+    return (
+    <div>
+    <Imgheader />
+    <Employeetable />
+    </div>
+    )
+}
+
+export default Content
