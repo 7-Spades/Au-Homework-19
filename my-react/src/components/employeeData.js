03a2941ccf95646887793adcff6647f00df436cc
7-Spades <edwin5106@gmail.com>
2020-05-12 17:49:54 -0500

    employee data

---
+++ my-react/src/components/employeeData.js
@@ -0,0 +1,24 @@
+const employees = [
+    {id: 1, firstname: 'John', lastname: 'Smith', career: 'Manager'},
+                {id: 2, firstname: 'Mark', lastname: 'Otto', career: 'Salesperson'},
+                {id: 3, firstname: 'Jaine', lastname: 'Rose', career: 'Overnight Stocker'},
+                {id: 4, firstname: 'Santos', lastname: 'Bryant', career: 'Customer Service Representative'},
+                {id: 5, firstname: 'Simeon', lastname: 'Sea', career: 'Warehouse Worker'},
+                {id: 6, firstname: 'Amani', lastname: 'Flores', career: 'Mechanic'},
+                {id: 7, firstname: 'Matilda', lastname: 'Friedman', career: 'Overnight Stocker'},
+                {id: 8, firstname: 'Aylin', lastname: 'Wong', career: 'Security Gaurd'},
+                {id: 9, firstname: 'Chris', lastname: 'Redfield', career: 'Warehouse Worker'},
+                {id: 10, firstname: 'Ellie', lastname: 'Conway', career: 'Salesperson'},
+                {id: 11, firstname: 'Christian', lastname: 'Chase', career: 'Assistant Manager'},
+                {id: 12, firstname: 'Jenna', lastname: 'Brennan', career: 'Customer Service Representative'},
+                {id: 13, firstname: 'Emery', lastname: 'Keith', career: 'Truck Driver'},
+                {id: 14, firstname: 'Carlos', lastname: 'Arellano', career: 'Overnight Stocker'},
+                {id: 15, firstname: 'Jordon', lastname: 'Nichols', career: 'Assistant Manager'},
+                {id: 16, firstname: 'Damaris', lastname: 'French', career: 'Warehouse Worker'},
+                {id: 17, firstname: 'Felix', lastname: 'Hart', career: 'Truck Driver'},
+                {id: 18, firstname: 'Paula', lastname: 'Moreno', career: 'Salesperson'},
+                {id: 19, firstname: 'Alfredo', lastname: 'Stephens', career: 'Warehouse Worker'},
+                {id: 20, firstname: 'April', lastname: 'Garrison', career: 'Customr Service Representative'}
+]
+
+export default employees
