d980b578d4fd094815ed794220db02cca96ef844
7-Spades <edwin5106@gmail.com>
2020-05-12 21:59:18 -0500

    added footer to website

--- my-react/src/components/content.js
+++ my-react/src/components/content.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Imgheader from './header'
 import Employeetable from './table'
+import Foot from './footer'
 
 function Content (){
     return (
     <div>
     <Imgheader />
     <Employeetable />
+    <Foot />
     </div>
     )
 }
---
+++ my-react/src/components/footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function foot(){
+    return (
+        <div className='footy'></div>
+    )
+}
+
+export default foot
